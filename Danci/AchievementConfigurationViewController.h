7e6dd01ace8b8f9682f7503889848c1c25992371
captainym <captainyuming@gmail.com>
2013-12-04 16:44:00 +0800

    Completing configuration ui...

---
+++ Danci/AchievementConfigurationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AchievementConfigurationViewController.h
+//  Danci
+//
+//  Created by zhenghao on 12/4/13.
+//  Copyright (c) 2013 mx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AchievementConfigurationViewController : UITableViewController
+
+@end
